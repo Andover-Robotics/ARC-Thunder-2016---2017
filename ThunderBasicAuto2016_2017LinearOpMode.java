e1e0c0eb82a8d7b4a8f22dd97fe90b1cd20c406a
Techcraft2 <la11010e@gmail.com>
2016-10-26 16:21:28 -0400

    Update ThunderBasicAuto2016_2017LinearOpMode.java
    Added pseudocode

--- ThunderBasicAuto2016_2017LinearOpMode.java
+++ ThunderBasicAuto2016_2017LinearOpMode.java
@@ -14,20 +14,23 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp(name="Basic Linear Autonomous", group="Autonomous")
 
 public class ThunderBasicAuto2016_2017LinearOpMode extends LinearOpMode {
-    /* Note:
+    /**
+     * Note:
      * When you extend OpMode, you must declare the methods init() and loop()
-     */
+     **/
 
 
-        /** Declaring electronics
-         * This can be done with a separate class and can make creating code much easier / simpler. */
-        private DcMotorController motorControllerP0;    // Motor Controller in port 0 of Core
-        private DcMotorController motorControllerP1;    // Motor Controller in port 1 of Core
+    /**
+     * Declaring electronics
+     * This can be done with a separate class and can make creating code much easier / simpler.
+     **/
+    private DcMotorController motorControllerP0;    // Motor Controller in port 0 of Core
+    private DcMotorController motorControllerP1;    // Motor Controller in port 1 of Core
 
-        private DcMotor motor1;                         // Motor 1: port 1 in Motor Controller 1
-        private DcMotor motor2;                         // Motor 2: port 2 in Motor Controller 1
-        private DcMotor motor3;                         // Motor 3: port 1 in Motor Controller 0
-        private DcMotor motor4;                         // Motor 4: port 2 in Motor Controller 0
+    private DcMotor motor1;                         // Motor 1: port 1 in Motor Controller 1
+    private DcMotor motor2;                         // Motor 2: port 2 in Motor Controller 1
+    private DcMotor motor3;                         // Motor 3: port 1 in Motor Controller 0
+    private DcMotor motor4;                         // Motor 4: port 2 in Motor Controller 0
 
     /* Declaring variables */
 
@@ -41,8 +44,10 @@ public class ThunderBasicAuto2016_2017LinearOpMode extends LinearOpMode {
         motor3 = hardwareMap.dcMotor.get("motorBack1");
         motor4 = hardwareMap.dcMotor.get("motorBack2");
 
-        /**Setting channel modes
-         *  When setting channel modes,  use the names that are declared to the motors. */
+        /**
+         * Setting channel modes
+         * When setting channel modes,  use the names that are declared to the motors.
+         **/
         motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -54,8 +59,33 @@ public class ThunderBasicAuto2016_2017LinearOpMode extends LinearOpMode {
         waitForStart();
 
         MoveForward(0.5, 2000 /*milliseconds*/); //Subject to change as we figure out a good standard speed
+
+        //This is all pseudocode that represents the flowchart we created earlier
+
+        /**
+        turnRight();
+        while(noWhite){
+            MoveForward(0.5, 2000);
+        }
+        while(notAlignedOnLine){
+            turnRight();
+        }
+        while(notDetectingColor){
+            MoveForward(0.5, 2000);
+        }
+        if(color = blue) {
+            press();
+        }
+        if(color = red){
+            turnLeft();
+            MoveForward();
+            turnRight();
+            press();
+         }
+         **/
+
     }
-    
+
     //Moves the robot forward based on the power given and the amount of time we want it to go
     public void MoveForward(double power, long time)throws InterruptedException{
         motor1.setPower(power);
@@ -63,6 +93,4 @@ public class ThunderBasicAuto2016_2017LinearOpMode extends LinearOpMode {
         motor3.setPower(power);
         motor4.setPower(power);
     }
-
-}
 
