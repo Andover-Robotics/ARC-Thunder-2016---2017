cf8bd66e73cd0a38e1a38bfb783d79664766a584
ROPit <Joshpk29@gmail.com>
2016-11-18 15:53:05 -0500

    Update Encoder.java

--- Encoder.java
+++ Encoder.java
@@ -8,23 +8,41 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.Range;
 
 /**
- *Created by Joshua Krinsky on 11/18/2016.
+ *Created by Joshua Krinsky a modification of Lake Yin's code on 11/18/2016.
  */
 
-@TeleOp(name="SweeperLauncherTest", group="TeleOp")
+@TeleOp(name="Encoder", group="TeleOp")
 
 public class SweeperLauncherTest extends OpMode {
 
-    private DcMotorController motorControllerP4;
     private DcMotor launcherMotor;
     
-        @Override
-    public void init() {
-        motorControllerP4 = hardwareMap.dcMotorController.get("MCP4"); //hardwaremapping the motor controller
-        launcherMotor = hardwareMap.dcMotor.get("motorLauncher"); //hardwaremapping the motor for the launcher
-
-
-        launcherMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        sweeperMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    @override
+    public void init ()
+    {
+     launcherMotor = hardwareMap.dcMotor.get("motorLauncher"); //hardwaremapping the motor for the launcher
+     launcherMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        
+        puvlic void LauncherEncoder()
+     double launcherPower = gamepad2.A; //sets the launcher power equal to press A button on the second controller
+        
+    launcherPower = Range.clip(launcherPower, -1, 1);//range of launcher motor values is between 0 and 1
+            }
+     
+        if(gamepad2.A==true){
+        LauncherMotor.setPower(1);
+            
+       }
+    else{
+    LauncherMotor.setPower(0);
     }
+    while (initIsActive())
+    {
+    telemetry.addData("text","run at speed");
+    telemerty.addData("power",LauncherMotor.getPower());
+    telemerty.addData("position". LauncherMotor.getCurrentPosition());
+    }
+        
+ 
+}
 
