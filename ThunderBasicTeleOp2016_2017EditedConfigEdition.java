578679f191c0f93177642a685238ff1f980bf57c
Big Kahuna Burger <curtislee0592@gmail.com>
2016-12-21 15:37:46 -0500

    Update ThunderBasicTeleOp2016_2017EditedConfigEdition.java
    Updated to add the strafe code.

--- ThunderBasicTeleOp2016_2017EditedConfigEdition.java
+++ ThunderBasicTeleOp2016_2017EditedConfigEdition.java
@@ -13,43 +13,49 @@ import com.qualcomm.robotcore.util.Range;
  * Created by citruseel on 10/14/2016.
  */
 
-@TeleOp(name="Thunder 2016-2017 TeleOp", group="TeleOp")
-
+@TeleOp(name="Thunder w2016-2017 TeleOp", group="TeleOp")
 public class ThunderBasicTeleOp2016_2017EditedConfigEdition extends OpMode {
 
-    private DcMotorController motorControllerP0;    // Motor Controller in port 0 of Core
-    private DcMotorController motorControllerP1;    // Motor Controller in port 1 of Core
-    private DcMotorController motorControllerP4;    // Motor Controller in port 4 of Core
-
-    private DcMotor motor1;                         // Motor 1: port 1 in Motor Controller 4
-    private DcMotor motor2;                         // Motor 2: port 2 in Motor Controller 4
-    private DcMotor motor3;                         // Motor 3: port 1 in Motor Controller 1
-    private DcMotor motor4;                         // Motor 4: port 2 in Motor Controller 1
-    private DcMotor sweeperMotor;                   // Sweeper motor: port 1 in Motor Controller 0
-    private DcMotor launcherMotor;                  // Launcher motor: port 2 in Motor Controller 0
-
+    private DcMotorController motorControllerL;    // left motor controllers
+    private DcMotorController motorControllerR;    // right motor controllers
+    private DcMotorController motorControllerA1;   // Scoring motor controller
+    private DcMotorController motorControllerA2;   // Scoring motor controller
     private ServoController servoController;
 
-    private Servo colorSensorServo;
+    private DcMotor motor1;
+    private DcMotor motor2;
+    private DcMotor motor3;
+    private DcMotor motor4;
+    private DcMotor sweeperMotor;
+    private DcMotor launcherMotor;
+    private DcMotor motor5;
 
-    private double servoposition = 0;//start position
+    private Servo servo;
+
+    private double servoposition = 0;
 
 
     @Override
     public void init() {
         /* Initializing and mapping electronics*/
-        motorControllerP0 = hardwareMap.dcMotorController.get("MCP0");
-        motorControllerP1 = hardwareMap.dcMotorController.get("MCP1");
-        motorControllerP4 = hardwareMap.dcMotorController.get("MCP4");
+        motorControllerL = hardwareMap.dcMotorController.get("MC_L");
+        motorControllerR = hardwareMap.dcMotorController.get("MC_R");
+        motorControllerA1 = hardwareMap.dcMotorController.get("MC_A1");
+        motorControllerA2 = hardwareMap.dcMotorController.get("MC_A2");
+        servoController = hardwareMap.servoController.get("SC");
 
 
-        motor1 = hardwareMap.dcMotor.get("motorFrontL");        //MCP4
-        motor2 = hardwareMap.dcMotor.get("motorFrontR");        //MCP4
-        motor3 = hardwareMap.dcMotor.get("motorBackL");         //MCP1          Back of motor 1
-        motor4 = hardwareMap.dcMotor.get("motorBackR");         //MCP1          Back of motor 2
+        motor1 = hardwareMap.dcMotor.get("motorFrontL");        //P0
+        motor2 = hardwareMap.dcMotor.get("motorFrontR");        //P1
+        motor3 = hardwareMap.dcMotor.get("motorBackL");         //P0
+        motor4 = hardwareMap.dcMotor.get("motorBackR");         //P1
 
-        launcherMotor = hardwareMap.dcMotor.get("motorLauncher"); //hardwaremapping the motor for the launcher MCP0 motor 1
-        sweeperMotor = hardwareMap.dcMotor.get("motorSweeper"); //hardwaremapping the motor for the sweeper MCP0 motor 2
+        servo = hardwareMap.servo.get("servo");
+
+        launcherMotor = hardwareMap.dcMotor.get("motorLauncher"); //P0
+        sweeperMotor = hardwareMap.dcMotor.get("motorSweeper"); //P1
+
+        motor5 = hardwareMap.dcMotor.get("motorStrafe");//P0 A2
 
         /*Setting channel modes*/
         motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -60,13 +66,11 @@ public class ThunderBasicTeleOp2016_2017EditedConfigEdition extends OpMode {
         launcherMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         sweeperMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
+        motor5.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
         motor2.setDirection(DcMotorSimple.Direction.REVERSE);
-        motor3.setDirection(DcMotorSimple.Direction.REVERSE);
-        //launcherMotor.setDirection(DcMotorSimple.Direction.REVERSE);                  We did this so that when the joystick is pushed up, the ball will launch upwards; when the joystick is pushed down, the ball will launch downwards
+        motor4.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        servoController = hardwareMap.servoController.get("SCP2"); //hardwaremapping the servo controller
-
-        colorSensorServo = hardwareMap.servo.get("servo"); //hardwaremapping the servo
 
 
 
@@ -74,15 +78,20 @@ public class ThunderBasicTeleOp2016_2017EditedConfigEdition extends OpMode {
 
     @Override
     public void loop() {                                          //constant loop that rechecks about every 20ms
-        double GearRatio = 0.25;                                  //We geared up from 80 teeth to 40 teeth: this is needed so that we don't go too fast)
 
-        double leftpower = gamepad1.left_stick_y * GearRatio;     //set's a value for power equal to the value of the joysticks for the left stick of the 1st controller
-        double rightpower = gamepad1.right_stick_y * GearRatio;   //set's a value for power equal to the value of the joysticks for the right stick of the 2nd controller
+        double acceleration = gamepad1.right_trigger > 0 ? 0.07: gamepad1.right_bumper ? 0.7 : 0.25;    //We geared up from 80 teeth to 40 teeth: this is needed so that we don't go too fast)
+
+        double leftpower = gamepad1.left_stick_y * acceleration;     //set's a value for power equal to the value of the joysticks for the left stick of the 1st controller
+        double rightpower = gamepad1.right_stick_y * acceleration;   //set's a value for power equal to the value of the joysticks for the right stick of the 2nd controller
 
         // value for the triggers is either 0.0 or 1.0
-        double sweeperPower = gamepad2.left_stick_y; //sets the sweeper power equal to the value of the joysticks for the left stick of the 2nd controller
+        double sweeperPower = gamepad1.left_bumper ? 1: gamepad1.left_trigger > 0 ? -1: gamepad2.left_stick_y; //sets the sweeper power equal to the value of the joysticks for the left stick of the 2nd controller
         double launcherPower = gamepad2.y ? 1 : gamepad2.a ? -1 : 0; //sets the power to the boolean next to it
         // "?" = if "thing before ?" then "thing after ?", else "after colon"
+
+        double strafePower = gamepad1.x ? 1 : gamepad1.b ? -1 : 0;
+
+        servoposition = gamepad2.right_trigger > 0 ? 1: 0;
 
         leftpower = Range.clip(leftpower, -1, 1);        //gamepad controllers have a value of 1 when you push it to its maximum foward
         rightpower = Range.clip(rightpower, -1, 1);      //range of power, min first then max
@@ -90,36 +99,30 @@ public class ThunderBasicTeleOp2016_2017EditedConfigEdition extends OpMode {
         sweeperPower = Range.clip(sweeperPower, -1, 1);//range of sweeper motor values is between 0 and 1
         launcherPower = Range.clip(launcherPower, -1, 1);//range of launcher motor values is between 0 and 1
 
-        motor1.setPower(rightpower);                    //connects the value for power to the actual power of the motors
-        motor2.setPower(leftpower);
+        strafePower = Range.clip(strafePower, -1, 1);//range of launcher motor values is between 0 and 1
+
+        servoposition = Range.clip(servoposition, 0, 1);
+
+        motor1.setPower(leftpower);                    //connects the value for power to the actual power of the motors
+        motor2.setPower(rightpower);
         motor3.setPower(leftpower);
         motor4.setPower(rightpower);
 
         sweeperMotor.setPower(sweeperPower);
         launcherMotor.setPower(launcherPower);
 
+        motor5.setPower(strafePower);
 
-        servoposition=Range.clip(servoposition, 0, 1);//range of servo values is between 0 and 1
+        servo.setPosition(servoposition);
 
-        if(gamepad2.x && !gamepad2.b) {                 //if(gamepad2.x) is the same as if(gamepad2.x == true)
-            servoposition = 0.05;
-        }
-        if (!gamepad2.x) {
-            if (gamepad2.b) {
-                servoposition = 0.95;
-            }
-            if (!gamepad2.b) {                          //if(!gamepad2.b) is the same as if(gamepad2.b == false)
-                servoposition = 0.5;
-            }
-        }
 
-        colorSensorServo.setPosition(servoposition); //constantly updates the servo's position every 20ms
-
-        telemetry.addData("LeftMotors", "Left Motor Power:" + leftpower);           //shows the data or text stated onto phone telemetry
-        telemetry.addData("RightMotors", "Right Motor Power:" + rightpower);
-        telemetry.addData("SweeperLauncherTest", "Sweeper Power: " + sweeperPower);
-        telemetry.addData("SweeperLauncherTest", "Launcher Power: " + launcherPower);
-        telemetry.addData("ServoTest", "Servo Postition: " + servoposition);
+        telemetry.addData("Acceleration:", acceleration);
+        telemetry.addData("Left Motor Power: ", leftpower);           //shows the data or text stated onto phone telemetry
+        telemetry.addData("Right Motor Power:", rightpower);
+        telemetry.addData("Sweeper Power: ", sweeperPower);
+        telemetry.addData("Launcher Power: ", launcherPower);
+        telemetry.addData("Strafe Power: ", strafePower);
+        telemetry.addData("Servo Position: ", servoposition);
     }
 
 }
